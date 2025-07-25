7f95c912f00328fe2b55c144d8ee4ee25af70643
mitch115 <m395l680@ku.edu>
2025-07-25 16:26:41 -0500

    added file seperationg for config data

---
+++ src/encrypted_config.h
@@ -0,0 +1,29 @@
+extern uint8_t config_iv_1[12];
+
+extern uint8_t encrypted_config_1[19];
+
+extern uint8_t additional_auth_data_1[19];
+
+extern uint8_t config_iv_2[12];
+
+extern uint8_t encrypted_config_2[29];
+
+extern uint8_t additional_auth_data_2[19];
+
+extern uint8_t config_iv_3[12];
+
+extern uint8_t encrypted_config_3[29];
+
+extern uint8_t additional_auth_data_3[19];
+
+extern uint8_t config_iv_4[12];
+
+extern uint8_t encrypted_config_4[21];
+
+extern uint8_t additional_auth_data_4[19];
+
+extern uint8_t config_iv_5[12];
+
+extern uint8_t encrypted_config_5[27];
+
+extern uint8_t additional_auth_data_5[19];
